1d024106664fea61c7823537b3667e43f4ef97e6
Jordan Jenkins <jordan@jordanjenkins.com>
2015-05-30 15:37:21 -0500

    add Passengerfile for Passenger standalone config, add url setting for subdomain usage in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'gradery.com/hc', port: 80 }
+  config.action_controller.default_url_options = { host: 'gradery.com/hc' }
 end
