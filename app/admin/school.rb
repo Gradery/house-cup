7e48231146b300c5486b6e4550cab6b4d589cf51
Jordan Jenkins <jordan@jordanjenkins.com>
2015-08-21 10:15:38 -0500

    Fix not being able to create a school if you are a superadmin. Fix superadmins not being able to create normal users.

--- app/admin/school.rb
+++ app/admin/school.rb
@@ -1,28 +1,34 @@
 ActiveAdmin.register School do
 
-permit_params :name, :about
+  permit_params :name, :url, :about
 
-index do
-	column :name
-	actions
-end
+  index do
+  	column :name
+  	actions
+  end
 
-form do |f|
+  form do |f|
     f.inputs "School" do
       f.input :name
+      f.input :url if current_admin_user.school_id.nil?
       f.input :about, as: :html_editor
     end
     f.actions
   end
 
-controller do
-  def scoped_collection
-    if !current_admin_user.school_id.nil?
-      School.where(:id => current_admin_user.school_id).all
-    else
-      School.all
+  controller do
+    def scoped_collection
+      if !current_admin_user.school_id.nil?
+        School.where(:id => current_admin_user.school_id).all
+      else
+        School.all
+      end
     end
+
+    # def permitted_params
+    #   params.permit :utf8, :_wysihtml5_mode, :authenticity_token, :commit,
+    #       school: [:name, :about, :url]
+    # end
   end
-end
 
 end
